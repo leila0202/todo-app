f4d59d40c42c6eec10e69ba6ae90b5a65771c4de
Leila Zrim <leilazrim@gmail.com>
2024-01-17 11:30:51 +0100

    [js - 07] [Step 43] Change the text of the "addOrUpdateTask" button

--- script.js
+++ script.js
@@ -59,6 +59,7 @@ const editTask = (buttonEl) => {
   titleInput.value = currentTask.title;
   dateInput.value = currentTask.date;
   descriptionInput.value = currentTask.description;
+  addOrUpdateTaskBtn.innerText = "Update Task";
 };
 
 const reset = () => {
