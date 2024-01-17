36dd3ab055d45d8bdaa4e630206dde0a872e35a2
Leila Zrim <leilazrim@gmail.com>
2024-01-17 10:26:17 +0100

    [js - 07] [Step 21] Add a paraghraph for the description to the template literal

--- script.js
+++ script.js
@@ -45,6 +45,7 @@ taskForm.addEventListener("submit", (e) => {
       <div class="task" id="${id}">
         <p><strong>Title: </strong>${title}</p>
         <p><strong>Date: </strong>${date}</p>
+        <p><strong>Description: </strong>${description}</p>
       </div>
     `;
   });
