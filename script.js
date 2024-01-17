295cbc42ed2234df690576a5d562701787b61952
Leila Zrim <leilazrim@gmail.com>
2024-01-17 10:10:30 +0100

    [js - 07] [Step 14] Add "title", "date" and "description" properties

--- script.js
+++ script.js
@@ -33,5 +33,8 @@ taskForm.addEventListener("submit", (e) => {
   const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
   const taskObj = {
     id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
+    title: titleInput.value,
+    date: dateInput.value,
+    description: descriptionInput.value,
   };
 });
