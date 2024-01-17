1ecf32f11f7e3ce78987d00bcae69ad54a868388
Leila Zrim <leilazrim@gmail.com>
2024-01-17 10:08:27 +0100

    [js - 07] [Step 13] Add Date to the id

--- script.js
+++ script.js
@@ -32,6 +32,6 @@ taskForm.addEventListener("submit", (e) => {
   e.preventDefault();
   const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
   const taskObj = {
-    id: `${titleInput.value.toLowerCase().split(" ").join("-")}`,
+    id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
   };
 });
