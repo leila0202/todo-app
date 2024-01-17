877fc34f5c70a86f4989056796d07526b0fccc47
Leila Zrim <leilazrim@gmail.com>
2024-01-17 11:44:56 +0100

    [js - 07] [Step 47.5] Add "myTaskArr"

--- script.js
+++ script.js
@@ -102,3 +102,9 @@ taskForm.addEventListener("submit", (e) => {
   e.preventDefault();
   addOrUpdateTask();
 });
+
+const myTaskArr = [
+  { task: "Walk the Dog", date: "22-04-2022" },
+  { task: "Read some books", date: "02-11-2023" },
+  { task: "Watch football", date: "10-08-2021" },
+];
