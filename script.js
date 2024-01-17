be5e0f7759258019514eb5f1f738ba4b5bfd6223
Leila Zrim <leilazrim@gmail.com>
2024-01-17 11:52:55 +0100

    [js - 07] [Step 52] Remove the "data" item from the localstorage

--- script.js
+++ script.js
@@ -111,6 +111,8 @@ const myTaskArr = [
 
 localStorage.setItem("data", JSON.stringify(myTaskArr));
 
+localStorage.removeItem("data");
+
 const getTaskArr = localStorage.getItem("data");
 console.log(getTaskArr);
 const getTaskArrObj = JSON.parse(localStorage.getItem("data"));
