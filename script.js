07a5a3543a354822aaa7bd0f62d9b362fc98bc58
Leila Zrim <leilazrim@gmail.com>
2024-01-17 12:02:21 +0100

    [js - 07] [Step 57] Retrieve the "taskData" from the localStorage

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const titleInput = document.getElementById("title-input");
 const dateInput = document.getElementById("date-input");
 const descriptionInput = document.getElementById("description-input");
 
-const taskData = [];
+const taskData = JSON.parse(localStorage.getItem("data")) || [];
 let currentTask = {};
 
 const addOrUpdateTask = () => {
