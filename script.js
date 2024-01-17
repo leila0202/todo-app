9acda2893ceda1f2519a177ed316050d79517d0e
Leila Zrim <leilazrim@gmail.com>
2024-01-17 10:45:56 +0100

    [js - 07] [Step 30] Split the "taskform submit event listener" into 2 functions

--- script.js
+++ script.js
@@ -13,6 +13,19 @@ const descriptionInput = document.getElementById("description-input");
 const taskData = [];
 let currentTask = {};
 
+const addOrUpdateTask = () => {
+  const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
+  const taskObj = {
+    id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
+    title: titleInput.value,
+    date: dateInput.value,
+    description: descriptionInput.value,
+  };
+  if (dataArrIndex === -1) {
+    taskData.unshift(taskObj);
+  }
+};
+
 const reset = () => {
   titleInput.value = "";
   dateInput.value = "";
@@ -44,16 +57,6 @@ discardBtn.addEventListener("click", () => {
 
 taskForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
-  const taskObj = {
-    id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
-    title: titleInput.value,
-    date: dateInput.value,
-    description: descriptionInput.value,
-  };
-  if (dataArrIndex === -1) {
-    taskData.unshift(taskObj);
-  }
   taskData.forEach(({ id, title, date, description }) => {
     tasksContainer.innerHTML += `
       <div class="task" id="${id}">
