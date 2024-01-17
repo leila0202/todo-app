14f67c36d00d0fccbcf0f55405fa10c4d440f39f
Leila Zrim <leilazrim@gmail.com>
2024-01-17 11:55:01 +0100

    [js - 07] [Step 54] Remove the "myTaskArr" and localstorage tests

--- script.js
+++ script.js
@@ -102,18 +102,3 @@ taskForm.addEventListener("submit", (e) => {
   e.preventDefault();
   addOrUpdateTask();
 });
-
-const myTaskArr = [
-  { task: "Walk the Dog", date: "22-04-2022" },
-  { task: "Read some books", date: "02-11-2023" },
-  { task: "Watch football", date: "10-08-2021" },
-];
-
-localStorage.setItem("data", JSON.stringify(myTaskArr));
-
-localStorage.clear();
-
-const getTaskArr = localStorage.getItem("data");
-console.log(getTaskArr);
-const getTaskArrObj = JSON.parse(localStorage.getItem("data"));
-console.log(getTaskArrObj);
