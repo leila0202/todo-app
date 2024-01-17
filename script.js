d478e75ee9dbffb62f3ae4b173e03ff53b6b105e
Leila Zrim <leilazrim@gmail.com>
2024-01-17 09:41:38 +0100

    [js - 07] [Step 08] Add click listener to the cancle button

--- script.js
+++ script.js
@@ -20,3 +20,5 @@ openTaskFormBtn.addEventListener("click", () =>
 closeTaskFormBtn.addEventListener("click", () =>
   confirmCloseDialog.showModal()
 );
+
+cancelBtn.addEventListener("click", () => confirmCloseDialog.close());
