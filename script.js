22d830a54e4074a46d10e3723ffeab20bd884b77
Leila Zrim <leilazrim@gmail.com>
2024-01-17 08:18:25 +0100

    [js - 07] [Step 02] Get closeTaskForm, addorupdateTaskForm and cancel button

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-/*Begin by accessing the task-form, confirm-close-dialog, and open-task-form-btn elements with the getElementById() method. Save them in the variables taskForm, confirmCloseDialog, and openTaskFormBtn.*/
+/*You need to access more elements with the getElementById() method. This time you need the close-task-form-btn, add-or-update-task-btn, and cancel-btn elements. Save them in the variables closeTaskFormBtn, addOrUpdateTaskBtn, and cancelBtn.*/
 const taskForm = document.getElementById("task-form");
 const confirmCloseDialog = document.getElementById("confirm-close-dialog");
 const openTaskFormBtn = document.getElementById("open-task-form-btn");
+const closeTaskFormBtn = document.getElementById("close-task-form-btn");
+const addOrUpdateTaskBtn = document.getElementById("add-or-update-task-btn");
+const cancelBtn = document.getElementById("cancel-btn");
